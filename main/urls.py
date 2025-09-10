da4b037e1e6d3b4e56d4a7bd8c7292efa52e1a40
cloudthegoat <awan.panjer@gmail.com>
2025-09-10 16:03:58 +0700

    commit

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,6 @@ app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('create-news/', create_news, name='create_news'),
+    path('news/<str:id>/', show_news, name='show_news'),
 ]
