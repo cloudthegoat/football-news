94543ba1186659ff7924029f92de8de8248313e7
cloudthegoat <awan.panjer@gmail.com>
2025-09-10 16:42:03 +0700

    test

--- main/views.py
+++ main/views.py
@@ -1,12 +1,17 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render, redirect, get_object_or_404
 from main.forms import NewsForm
-from main.models import News
+from main.models import News    
 
 def show_main(request):
+    news_list = News.objects.all()
+
     context = {
-        'npm' : '2406420772',
+        'npm' : '24006420772',
         'name': 'Kadek Ngurah Septyawan Chandra Diputra',
-        'class': 'PBP C'
+        'class': 'PBP C',
+        'news_list': news_list
     }
 
     return render(request, "main.html", context)
@@ -30,3 +35,29 @@ def show_news(request, id):
     }
 
     return render(request, "news_detail.html", context)
+
+def show_xml(request):
+     news_list = News.objects.all()
+     xml_data = serializers.serialize("xml", news_list)
+     return HttpResponse(xml_data, content_type="application/xml")
+
+def show_json(request):
+    news_list = News.objects.all()
+    json_data = serializers.serialize("json", news_list)
+    return HttpResponse(json_data, content_type="application/json")
+
+def show_xml_by_id(request, news_id):
+   try:
+       news_item = News.objects.filter(pk=news_id)
+       xml_data = serializers.serialize("xml", news_item)
+       return HttpResponse(xml_data, content_type="application/xml")
+   except News.DoesNotExist:
+       return HttpResponse(status=404)
+
+def show_json_by_id(request, news_id):
+   try:
+       news_item = News.objects.get(pk=news_id)
+       json_data = serializers.serialize("json", [news_item])
+       return HttpResponse(json_data, content_type="application/json")
+   except News.DoesNotExist:
+       return HttpResponse(status=404)
